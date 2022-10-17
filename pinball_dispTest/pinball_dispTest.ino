946b77d36c401a83351b95809af85be9a8120665
Brian <56484785+160rpm@users.noreply.github.com>
2022-10-17 02:05:51 +0200

    Update pinball_dispTest.ino

--- pinball_dispTest/pinball_dispTest.ino
+++ pinball_dispTest/pinball_dispTest.ino
@@ -22,8 +22,9 @@ int lampLatchSel[] = {18,19,20};
 int lampLatchEnable = 21;
 
 
-int switchIN[] = {22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40, 41,42, 43, 44, 45, 46, 47, 48, 49, 50,51,52,53};  int inputCnt = 32;
-//Only 22-40 used atm.
+int switchIN[] = {22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40, 41,42, 43, 44, 45, 46, 47, 48, 49, 50,51,52,53};  
+int inputCnt = 19; //Only 22-40 used atm.
+
 int coinSW = 22;
 int startSW = 23;
 int troughSW = 24;
